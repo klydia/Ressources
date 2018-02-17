1ad668aa94dabb3b4b6190f7483ae5ea4eede5f3
hphp <hphp@hphp-PC>
2018-02-17 14:48:16 +0100

    modifier materiel

--- Ressources/src/glp/Materiel.java
+++ Ressources/src/glp/Materiel.java
@@ -1,56 +1,38 @@
-package glp;
+ package glp;
 
 public class Materiel {
-	private int pc;
-	private int projecteur;
-	private int tableau;
-	private int micro;
+	private String nomMateriel;
+	private int NbMateriel;
 	
 	
-	public Materiel() {
-		this.pc=pc;
-		this.projecteur=projecteur;
-		this.tableau=tableau;
-		this.micro=micro;
-	}
-
-
-	public int getPc() {
-		return pc;
-	}
-
-	public void setPc(int pc) {
-		this.pc = pc;
-	}
-
-	public int getProjecteur() {
-		return projecteur;
-	}
-
-
-	public void setProjecteur(int projecteur) {
-		this.projecteur = projecteur;
-	}
-
-
-	public int getTableau() {
-		return tableau;
-	}
-
-
-	public void setTableau(int tableau) {
-		this.tableau = tableau;
-	}
-
-
-	public int getMicro() {
-		return micro;
-	}
-
-
-	public void setMicro(int micro) {
-		this.micro = micro;
+	public Materiel(String nomMateriel, int NbMateriel) {
+		this.nomMateriel= nomMateriel;
+		this.NbMateriel=NbMateriel;
+		
 	}
 	
+	public void ajouterMateriel(int index) {
+		this.NbMateriel += index;
+	}
+	public void supprimerMateriel(int j) {
+		this.NbMateriel -= j;
+	}
+
+	public String getNomMateriel() {
+		return nomMateriel;
+	}
+
+	public void setNomMateriel(String nomMateriel) {
+		this.nomMateriel = nomMateriel;
+	}
+
+	public int getNbMateriel() {
+		return NbMateriel;
+	}
+
+	public void setNbMateriel(int nbMateriel) {
+		NbMateriel = nbMateriel;
+	}
+
 
 }
