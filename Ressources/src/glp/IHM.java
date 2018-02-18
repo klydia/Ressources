86b927cd066f9ef85ecf0b2c6b18a88db94c4700
hphp <hphp@hphp-PC>
2018-02-18 15:16:28 +0100

    modifier IHM

--- Ressources/src/glp/IHM.java
+++ Ressources/src/glp/IHM.java
@@ -2,7 +2,7 @@ package glp;
 
 public class IHM {
 
-public class newJFrame extends javax.swing.JFrame {
+public static class newJFrame extends javax.swing.JFrame {
 
     public newJFrame() {
         initComponents();
@@ -285,7 +285,7 @@ public class newJFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }                                          
 
-    public void main(String args[]) {
+    public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
